bec153161413f727f234376f971032a43091bfb4
Siphalor <info@siphalor.de>
2022-06-27 18:20:07 +0200

    Update to 1.19

--- src/main/java/de/siphalor/wanderingcollector/mixin/MixinServerPlayerEntity.java
+++ src/main/java/de/siphalor/wanderingcollector/mixin/MixinServerPlayerEntity.java
@@ -27,9 +27,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -46,8 +48,8 @@ public abstract class MixinServerPlayerEntity extends PlayerEntity implements IS
 	@Unique
 	private ArrayList<NbtCompound> lostStacks = new ArrayList<>();
 
-	public MixinServerPlayerEntity(World world, BlockPos pos, float yaw, GameProfile profile) {
-		super(world, pos, yaw, profile);
+	public MixinServerPlayerEntity(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
+		super(world, pos, yaw, gameProfile, publicKey);
 	}
 
 	@Inject(
